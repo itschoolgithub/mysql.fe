a77dad1e12cde95daeb63b86b3b5f4b834478cbf
itschool <itschool@tou.edu.kz>
2026-02-14 15:08:41 +0500

    init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+{{ title }}
+<button
+@click="loadTitle"
+>Загрузить данные</button>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+data() {
+    return {
+        title: '',
+    }
+},
+methods: {
+    async loadTitle() {
+        const result = await axios.get('http://mysql.be/index.php')
+        this.title = result.data.title
+    }
+}
+}
+</script>
